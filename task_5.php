d02ad98ffbdb3143e704699d67e2af32e6ad1156
RezoanulHasan <rezoanulhasan96@gmail.com>
2023-09-25 22:44:33 +0600

    add 5

---
+++ task_5.php
@@ -0,0 +1,19 @@
+<?php
+
+function generatePassword($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+';
+    $password = '';
+
+    for ($i = 0; $i < $length; $i++) {
+        $randomIndex = rand(0, strlen($characters) - 1);
+        $password .= $characters[$randomIndex];
+    }
+
+    return $password;
+}
+
+$generatedPassword = generatePassword(12);
+
+echo "Generated Password: $generatedPassword\n";
+
+?>
